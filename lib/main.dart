194b189b2187462b15b2c06048f2bda9f2249138
Shubrata-Datta <shubratadatta529@gmail.com>
2026-02-27 11:54:55 +0600

    Abc

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());//runApp() is a function that takes a widget and makes it the root of the widget tree. It also initializes the Flutter framework and starts the app.
+}
+class MyApp extends StatelessWidget {//StatelessWidget is a widget that does not require mutable state. It is used when the widget does not need to change over time.
+  const MyApp({super.key});//stl is a shortcut for creating a stateless widget.It creates a class that extends StatelessWidget and has a constructor that takes a key.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();//MaterialApp is a widget that wraps a number of widgets that are commonly required for material design applications. It is used to set up the app's theme, routes, and other properties.
+    
+  }
+}
